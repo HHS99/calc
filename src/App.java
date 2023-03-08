ee12941fd7e9007e55c60003051967fd29852363
HHS <hs.han@2ps.co.jp>
2023-03-08 22:52:20 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("han Hello, World!");
+        System.out.println("hello world");
+    }
+}
